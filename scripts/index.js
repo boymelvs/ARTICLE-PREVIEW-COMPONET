5a77428613703adccd524dcaa6e1960f4ce18474
boymelvs <ambo_dacoycoy@yahoo.com>
2021-11-06 21:22:21 +0300

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const share = document.querySelector(".share_arrow");
+const arrow = document.querySelector(".arrow_icon");
 const showShareButton = document.querySelector(".share_container");
 
 share.addEventListener("click", () => {
-  showShareButton.classList.toggle("active");
+   showShareButton.classList.toggle("active");
+   arrow.classList.toggle("active");
+   share.classList.toggle("active");
 });
